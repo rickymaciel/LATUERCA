ef4a168a00160ed42d8316fb8639117bf0b942d9
rickymaciel <rmacielb3@gmail.com>
2016-05-11 15:59:07 -0400

    Nuevo Version LaTuerca

---
+++ LaTuerca/Migrations/201605111821122_ManageViewModels.cs
@@ -0,0 +1,16 @@
+namespace LaTuerca.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ManageViewModels : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ LaTuerca/Models/Marca.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LaTuerca.Models
+{
+    public class Marca
+    {
+        public Marca()
+        {
+            Repuestos = new List<Repuesto>();
+        }
+        [HiddenInput(DisplayValue = true)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar el nombre")]
+        [StringLength(100, ErrorMessage = "El { 0} debe ser al menos {2} caracteres.", MinimumLength = 6)]
+        public string Nombre { get; set; }
+
+        public Boolean Estado { get; set; }
+
+        public virtual ICollection<Repuesto> Repuestos { get; set; }
+
+    }
+}
